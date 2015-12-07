1d2b55746468af75c6f1bd56e177c7219223fd2a
Rich Allen <rallen@taoti.com>
2015-12-07 11:40:14 -0500

    update

---
+++ web/drush/site-aliases/lexrants.aliases.drushrc.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * A drupal aliase file example.
+ *
+ * @file
+ *
+ * PHP Version 7.0.0
+ *
+ */
+
+$home = drush_server_home();
+
+$aliases['local'] = array(
+  'env' => 'dev',
+  'root' => '/Users/rallen/clones/lexrants.me/web',
+  'uri' => 'http://127.0.0.1:8888',
+  'path-aliases' => array(
+    '%files' => 'sites/default/files',
+    '%drush-script' => '../vendor/bin/drush',
+  )
+);
+
+$aliases['live'] = array(
+  'env' => 'dev',
+  'root' => '/var/www/lexrants.me/web',
+  'uri' => 'https://lexrants.me',
+  'remote-user' => 'root',
+  'remote-host' => 'lexrants.me',
+  'path-aliases' => array(
+    '%files' => 'sites/default/files',
+    '%drush-script' => '/var/www/lexrants.me/vendor/bin/drush',
+  )
+);
